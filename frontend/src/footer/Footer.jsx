e9a8e3e25b5bb82d5cbb8c0ae28e02010ffd648b
pankajsalunke <pankajsalunke305@gmail.com>
2025-08-12 12:23:16 +0530

    changes

--- frontend/src/footer/Footer.jsx
+++ frontend/src/footer/Footer.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      Footer
+      <h1>new line</h1>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
