b29f52fc5af3afc474a2dea50adbffeb1533c347
pankajsalunke <pankajsalunke305@gmail.com>
2026-01-21 14:51:02 +0530

    add vercel url in cors

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,15 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://employee-task-management-rosy.vercel.app/",
+    credentials: true,
+  }),
+);
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
-// app.use(express.
-app.use(express.static("public"));
 app.use(cookieParser());
 
 // import routes
